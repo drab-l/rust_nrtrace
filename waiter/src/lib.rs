bbee8d39fc5718552fb5ff0eb012fc4e33fbf2e7
drab <23292996+drab-l@users.noreply.github.com>
2023-04-15 09:51:44 +0900

    add waiter document

--- waiter/src/lib.rs
+++ waiter/src/lib.rs
@@ -1,3 +1,4 @@
+//! wrapper for wait C function
 mod c {
     extern "C" {
         pub fn waitpid(pid: types::Pid, status: *mut types::SInt, options: types::SInt) -> types::Pid;
@@ -6,6 +7,9 @@ mod c {
     pub const WSTOPPED: types::SInt = 0x00000002;
 }
 
+/// wapper for wait(pid, &status, __WALL), return status or errno
+/// # Arguments
+/// * `pid` - A process ID of wait target
 pub fn wait_one(pid: types::Pid) -> std::io::Result<types::SInt> {
     let mut status: types::SInt = 0;
     match unsafe { c::waitpid(pid, &mut status, c::__WALL) } {
@@ -15,6 +19,9 @@ pub fn wait_one(pid: types::Pid) -> std::io::Result<types::SInt> {
     }
 }
 
+/// wapper for wait(pid, &status, WSTOPPED), return status or errno
+/// # Arguments
+/// * `pid` - A process ID of wait target
 pub fn wait_one_stop(pid: types::Pid) -> std::io::Result<types::SInt> {
     let mut status: types::SInt = 0;
     match unsafe { c::waitpid(pid, &mut status, c::WSTOPPED) } {
@@ -24,6 +31,7 @@ pub fn wait_one_stop(pid: types::Pid) -> std::io::Result<types::SInt> {
     }
 }
 
+/// wapper for wait(-1, &status, __WALL), return status or errno
 pub fn wait_any() -> std::io::Result<(types::Pid, types::SInt)> {
     let mut status: types::SInt = 0;
     match unsafe { c::waitpid(-1 as types::Pid, &mut status, c::__WALL) } {
